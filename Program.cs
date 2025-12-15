38ce5766f05231c1aa7090b00e960dd5d0500ff2
atharalishigri <bsef22m045@pucit.edu.pk>
2025-12-16 01:20:35 +0500

    conflict 1 resolved

--- Program.cs
+++ Program.cs
@@ -1,30 +1,56 @@
 using Artify.Api.Data;
 using Artify.Api.Models;
+using Artify.Api.Repositories.Interfaces;
+using Artify.Api.Repositories.Implementations;
+using Artify.Api.Services.Interfaces;
+using Artify.Api.Services.Implementations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
+builder.Services.AddScoped<IArtistRepository, ArtistRepository>();
+builder.Services.AddScoped<IArtServiceRepository, ArtServiceRepository>();
+builder.Services.AddScoped<IArtworkRepository, ArtworkRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IProtectionRepository, ProtectionRepository>();
+builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
+
+
+builder.Services.AddScoped<IArtistDashboardService, ArtistDashboardService>();
+builder.Services.AddScoped<IArtistProfileService, ArtistProfileService>();
+builder.Services.AddScoped<IArtServiceListingService, ArtServiceListingService>();
+builder.Services.AddScoped<IArtworkService, ArtworkService>();
+builder.Services.AddScoped<IProtectionService, ProtectionService>();
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseHttpsRedirection();
 }
 
 app.UseHttpsRedirection();
