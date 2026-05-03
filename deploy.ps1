9d3f07602e59b31b75242a95f172986f6d77ceb1
atharalishigri <bsef22m045@pucit.edu.pk>
2026-05-04 01:09:17 +0500

    modified .gitignore

--- deploy.ps1
+++ deploy.ps1
@@ -10,6 +10,11 @@ Copy-Item -Path "dist\*" -Destination "..\wwwroot\" -Recurse -Force
 cd ..
 git add .
 git commit -m $message
+
+Write-Host "Pushing to GitHub..." -ForegroundColor Cyan
+git push origin main
+
+Write-Host "Pushing to Azure..." -ForegroundColor Cyan
 git push azure main:master
 
-Write-Host "Deployment complete! " -ForegroundColor Green
+Write-Host "Deployment complete!" -ForegroundColor Green
